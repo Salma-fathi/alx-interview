a589b3490d1ba0b3f45929cefa4fdbdb9f79932a
Salma-fathi <salma89040@gmail.com>
2024-11-20 12:41:29 +0400

    update new

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,17 +1,44 @@
 #!/usr/bin/node
 
 const request = require('request');
+const { text } = require('stream/consumers');
+const util = require('util');
 
-request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
-  if (err) throw err;
-  const actors = JSON.parse(body).characters;
-  exactOrder(actors, 0);
-});
-const exactOrder = (actors, x) => {
-  if (x === actors.length) return;
-  request(actors[x], function (err, res, body) {
-    if (err) throw err;
-    console.log(JSON.parse(body).name);
-    exactOrder(actors, x + 1);
-  });
+// Convert request to promise-based
+const requestAsync = util.promisify(request);
+
+// Base URL for Star Wars API
+const BASE_URL = 'https://swapi.dev/api';
+
+async function getCharactersFromMovie(movieId) {
+  try {
+    // Get movie data
+    const movieResponse = await requestAsync(`${BASE_URL}/films/${movieId}/`);
+    const movieData = JSON.parse(movieResponse.body);
+    const characterUrls = movieData.characters;
+
+    // Get all characters
+    const characterPromises = characterUrls.map(url => requestAsync(url));
+    const characterResponses = await Promise.all(characterPromises);
+
+    // Extract and print character names in order
+    characterResponses.forEach(response => {
+      const character = JSON.parse(response.body);
+      console.log(character.name);
+    });
+  } catch (error) {
+    console.error('Error:', error.message);
+    process.exit(1);
+  }
 }
+
+// Get movie ID from command line argument
+const movieId = process.argv[2];
+
+if (!movieId) {
+  console.error('Please provide a movie ID');
+  process.exit(1);
+}
+
+// Execute the main function
+getCharactersFromMovie(movieId);
