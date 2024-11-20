57aca57b810c6ae83846644453e4691806cbffb1
Salma-fathi <salma89040@gmail.com>
2024-11-20 12:45:04 +0400

    new update

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,44 +1,36 @@
 #!/usr/bin/node
 
 const request = require('request');
-const { text } = require('stream/consumers');
-const util = require('util');
 
-// Convert request to promise-based
-const requestAsync = util.promisify(request);
+function fetchCharacters (movieId) {
+  const url = `https://swapi.dev/api/films/${movieId}/`;
 
-// Base URL for Star Wars API
-const BASE_URL = 'https://swapi.dev/api';
+  request(url, (error, response, body) => {
+    if (error) {
+      console.error(error);
+      return;
+    }
 
-async function getCharactersFromMovie(movieId) {
-  try {
-    // Get movie data
-    const movieResponse = await requestAsync(`${BASE_URL}/films/${movieId}/`);
-    const movieData = JSON.parse(movieResponse.body);
-    const characterUrls = movieData.characters;
-
-    // Get all characters
-    const characterPromises = characterUrls.map(url => requestAsync(url));
-    const characterResponses = await Promise.all(characterPromises);
-
-    // Extract and print character names in order
-    characterResponses.forEach(response => {
-      const character = JSON.parse(response.body);
-      console.log(character.name);
+    const characters = JSON.parse(body).characters;
+    const characterPromises = characters.map(url => {
+      return new Promise((resolve, reject) => {
+        request(url, (error, response, body) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(JSON.parse(body).name);
+          }
+        });
+      });
     });
-  } catch (error) {
-    console.error('Error:', error.message);
-    process.exit(1);
-  }
+
+    Promise.all(characterPromises)
+      .then(names => {
+        names.forEach(name => console.log(name));
+      })
+      .catch(error => console.error(error));
+  });
 }
 
-// Get movie ID from command line argument
 const movieId = process.argv[2];
-
-if (!movieId) {
-  console.error('Please provide a movie ID');
-  process.exit(1);
-}
-
-// Execute the main function
-getCharactersFromMovie(movieId);
+fetchCharacters(movieId);
